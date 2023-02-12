eed2c87ce193945781a2c780f2a78808de96a000
Ashikurjaman <ashikbur007@gmail.com>
2023-02-12 17:18:16 +0600

    php -step-03-complete

--- loop.php
+++ loop.php
@@ -15,7 +15,7 @@ for ($i = 1; $i < 100; $i++) {
 echo PHP_EOL;
 
 $i = 1;
-while($i<10){
+while ($i < 10) {
     $i++;
     // echo $i;
     echo PHP_EOL;
@@ -27,88 +27,78 @@ do {
     // echo $i.PHP_EOL;
 } while ($i < 10);
 
-
-
 // // multiple stepping
 
-for($i=10,$j=1; $i>0;$i--,$j++){
-    echo $i.":".$j;
+for ($i = 10, $j = 1; $i > 0; $i--, $j++) {
+    echo $i . ":" . $j;
     echo PHP_EOL;
 }
 
-
-for($i=1,$j=1; $i<100; $i+=7,$j+=11){
-    print $i."\n";
-    echo $j<100 ? $j."\n" : "";
+for ($i = 1, $j = 1; $i < 100; $i += 7, $j += 11) {
+    print $i . "\n";
+    echo $j < 100 ? $j . "\n" : "";
 
 }
-            // how to change sum value and add sum value;
-$sum= 0;
-for($i=1;$i<=30;$i++){
-    $sum+=$i;
+// how to change sum value and add sum value;
+$sum = 0;
+for ($i = 1; $i <= 30; $i++) {
+    $sum += $i;
 }
 
 // echo $sum;
 
 echo PHP_EOL;
 
-            //    factorial calculation 
+//    factorial calculation
 /* $x=6;
 $n=1;
 for($i=1;$i<=$x-1;$i++){
-    $n*=($i+1);
-   
+$n*=($i+1);
 
 }
 echo $n; */
 
-
-// continue and break 
+// continue and break
 
 /* for($i=0;$i<10;$i++){
-    if($i==6){
-        continue;
-    }
-    print $i;
-    print PHP_EOL; // php end of the line
+if($i==6){
+continue;
+}
+print $i;
+print PHP_EOL; // php end of the line
 } */
-
 
 /* $veryOld=0;
 $old=1;
 $new=1;
-for ($i=0; $i <10 ; $i++) { 
-    echo $veryOld. PHP_EOL;
-    $old = $new;
-    $new= $new + $veryOld;
-    $veryOld = $old; 
+for ($i=0; $i <10 ; $i++) {
+echo $veryOld. PHP_EOL;
+$old = $new;
+$new= $new + $veryOld;
+$veryOld = $old;
 } */
 
-
-$num=14;
-if(isEven($num)){
+$num = 14;
+if (isEven($num)) {
     echo "$num is a even number";
-}else{
+} else {
     echo "$num is a odd number";
 }
 echo PHP_EOL;
 
-$X=5;
+$X = 5;
 echo fab($X);
 echo PHP_EOL;
 
-
-$x= "tea";
-$y="";
-serve($x,$y);
+$x = "tea";
+$y = "";
+serve($x, $y);
 
 echo PHP_EOL;
 printN(1);
 
+echo PHP_EOL;
+printnumber(20, 30);
 
 echo PHP_EOL;
-printnumber(20,30);
-
-
-echo PHP_EOL;
-fabonacci(0,1,1,15);
+fabonacci(0, 1, 1, 15);
